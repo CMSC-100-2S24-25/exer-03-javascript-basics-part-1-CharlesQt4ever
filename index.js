33de3f0829cb550b0e2d052bc46b206869354291
Charles <cpdelosreyes@up.edu.ph>
2025-02-18 23:39:09 +0800

    Updated the functionalities

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class PasswordManagement {
-    constructor(password1, password2) {
+    constructor() {
         this.storedPassword = "";
     }
 
-    validatePassword(password1, password2 ) {
+    validatePassword(password1, password2) {
         let hasLowerCase = false;
         let hasUpperCase = false;
         let hasNumber = false;
@@ -11,81 +11,82 @@ class PasswordManagement {
         const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
             "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-        const lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
-            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
             
-        const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-
-        if (password1 !== password2){
+        const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        
+        //check if password match
+        if (password1 !== password2) {
             console.log("The password does not match");
             return false;
-        } else if(password1.length < 8) {
-            console.log("The password must have atleast 8 characters");
+        //check minimum password length
+        } else if (password1.length < 8) {
+            console.log("The password must have at least 8 characters");
             return false;
         } else {
-            for(let i = 0; i < password2.length; i++) {
+            //iterate in each letter
+            for (let i = 0; i < password2.length; i++) {
                 let char = password2[i];
 
-                for(let j = 0; j < hasUpperCase.length; j++){
-                    if(char === hasUpperCase[j]){
+                //check if uppercase letter
+                for (let j = 0; j < upperCase.length; j++) {
+                    if (char === upperCase[j]) {
                         hasUpperCase = true;
                         break;
                     }
                 }
-                for(let j = 0; j < hasLowerCase.length; j++){
-                    if(char === hasLowerCase[j]){
+                //check if lowercase letter
+                for (let j = 0; j < lowerCase.length; j++) {
+                    if (char === lowerCase[j]) {
                         hasLowerCase = true;
                         break;
                     }
                 }
-                for(let j = 0; j < hasNumber.length; j++){
-                    if(chastoredr === hasNumber[j]){
+                //check if number
+                for (let j = 0; j < numbers.length; j++) {
+                    if (char === numbers[j]) {
                         hasNumber = true;
                         break;
                     }
                 }
             }
-            return hasUpperCase && hasLowerCase && hasNumber;
+            if (hasUpperCase && hasLowerCase && hasNumber) {
+                return true;
+            } else {
+                console.log("Password must contain at least one uppercase letter, one lowercase letter, and one number.");
+                return false;
+            }
         }
     }
-
     
     reversedPassword(password2) {
         let reversedPassword = "";
 
-        for(let i = password2.length - 1; i >= 0; i--) {
+        //iterate in reverse order
+        for (let i = password2.length - 1; i >= 0; i--) {
             reversedPassword += password2[i];
         }
 
         return reversedPassword;
     }
 
-    storePassword(names, password1, password2) {
-        if(this.validatePassword(password1, password2)){
-            this.storedPassword = this.reversedPassword();
-            console.log("Password is succesfully stored.");
-            return this.storePassword;
+    storePassword(name, password1, password2) {
+        if (this.validatePassword(password1, password2)) {
+            this.storedPassword = this.reversedPassword(password1);
+            console.log("Password successfully stored.");
+            return { name: name, newPassword: this.storedPassword };
         } else {
-            console.log("NOT VALID!");
+            console.log("Password is not valid.");
+            return { name: name, newPassword: password1 };
         }
-        return names;
     }
 }
 
 const passwordManagement = new PasswordManagement();
-const password1 = "Hello123";
-const password2 = "Hello123";
+
+const password1 = "Pass12345";
+const password2 = "Pass12345";
 const names = "John";
 
-//console.log("Storing password...");
-//passwordManagement.storePassword(names, password1, password2);
-
-if(passwordManagement.validatePassword(password1, password2)) {
-    console.log("New Password: ", passwordManagement.reversedPassword());
-} else {
-    console.log("No New Password");
-}
-
-
-
-
+console.log(passwordManagement.storePassword(names, password1, password2));
